5847d1dfb607fb2e89d392c95af8aada33348f14
vinaydeep <vinaydeep@192.168.250.24>
2023-11-14 18:57:35 -0500

    4 - basic code

--- Selenium/src/test/java/com/vinay/Selenium/Main.java
+++ Selenium/src/test/java/com/vinay/Selenium/Main.java
@@ -1,5 +1,17 @@
 package com.vinay.Selenium;
 
-public class Main {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
+public class Main {
+	public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "D:\\myfiles\\Lakehead Studies\\my\\resources\\testing\\driver\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        String pageTitle = driver.getTitle();
+        System.out.println("Page Title: " + pageTitle);
+
+	}
 }
